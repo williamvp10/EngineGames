9dfb0d2c68127a3c3c7a9b06f45886cd2b9d710d
william <william@LAPTOP-6SPV04V0>
2018-05-30 22:16:09 -0500

    chat 4

--- src/main/webapp/js/chats.js
+++ src/main/webapp/js/chats.js
@@ -2,7 +2,7 @@ var ws;
 function connect() {
     var username = document.getElementById("nickname").value;
     
-    ws = new WebSocket( document.location.host + "/chat/" + username);
+    ws = new WebSocket("http://"+ document.location.host + "/myApp/chat/" + username);
     ws.onmessage = function (event) {
         var str1 = "nuevo amigo";
         var message = JSON.parse(event.data);
